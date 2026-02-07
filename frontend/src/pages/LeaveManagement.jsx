25b7eaf32d782c6bf023668097e4db2cb79fcf16
mediapower13 <nurudeenbolaman@gmail.com>
2026-02-07 11:21:14 +0100

    employment

--- frontend/src/pages/LeaveManagement.jsx
+++ frontend/src/pages/LeaveManagement.jsx
@@ -21,6 +21,7 @@ const LeaveManagement = () => {
   useEffect(() => {
     fetchEmployees();
     fetchLeaveRequests();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const fetchEmployees = async () => {
