be5b672084fe3c9e14b49c7bc90519c8866a03ae
mediapower13 <nurudeenbolaman@gmail.com>
2026-02-07 11:20:14 +0100

    employment

--- frontend/src/pages/Attendance.jsx
+++ frontend/src/pages/Attendance.jsx
@@ -22,6 +22,7 @@ const Attendance = () => {
   useEffect(() => {
     fetchEmployees();
     fetchAttendances();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const fetchEmployees = async () => {
